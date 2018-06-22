df297b13ed5f0a665665351186b73b57a4365a41
Aaron Whittington <aaron.whittington@ratepay.com>
2018-06-22 17:13:07 +0200

    feature/SHPWR-140_bidirectionality_without_events working on alternative to history based updated...

--- Bootstrapping/Events/UpdateTransactionsSubscriber.php
+++ Bootstrapping/Events/UpdateTransactionsSubscriber.php
@@ -58,30 +58,43 @@ class Shopware_Plugins_Frontend_RpayRatePay_Bootstrapping_Events_UpdateTransacti
      */
     private function findCandiateOrdersForUpdate($config)
     {
-        $paymentMethods = Shopware_Plugins_Frontend_RpayRatePay_Bootstrapping_Events_OrderOperationsSubscriber::PAYMENT_METHODS;
-        $orderStatus = [
-            $config['RatePayFullDelivery'],
-            $config['RatePayFullCancellation'],
-            $config['RatePayFullReturn'],
-        ];
+        //because we may not have any entries in s_order_history
+        //and orders may take a long time
+        //we limit to pretty far back in the past
+        $date = new DateTime();
+        $date->sub(new DateInterval('P1Y'));
+        $orderDate = $date->format('Y-m-d H:i:s');
 
-        $changeDate = $this->getLastUpdateDate();
+        $query = 'SELECT order_id FROM 
+                    (SELECT o.id as order_id,
+                     o.ordertime,
+                     o.status, 
+                     SUM(od.quantity) AS total_items,
+                     SUM(rop.delivered) AS total_rp_shipped,
+                     SUM(rop.cancelled) AS total_rp_cancelled,
+                     SUM(rop.returned) AS total_rp_returned,
+                     SUM(rop.returned + rop.cancelled) AS total_rp_cancelled_or_returned, 
+                     COUNT(rop.s_order_details_id) AS ct_rp
+                     FROM s_order o
+                    LEFT JOIN s_order_details od ON od.orderID = o.id
+                    LEFT JOIN s_order_history oh ON oh.orderID = o.id
+                    LEFT JOIN rpay_ratepay_order_positions rop ON rop.s_order_details_id = od.id
+                    GROUP BY o.id
+                    ORDER BY o.id DESC) AS subquery
+                WHERE ct_rp > 0
+                AND (
+                    (status = :fullDelivery AND total_rp_shipped = 0) OR
+                    (status = :fullCancellation AND total_rp_cancelled_or_returned != total_items) OR
+                    (status = :fullReturn AND total_rp_cancelled_or_returned != total_items)
+                )
+                AND ordertime > :orderDate';
 
-        if (empty($changeDate)) {
-            $date = new DateTime();
-            $date->sub(new DateInterval('P1D'));
-            $changeDate = $date->format('Y-m-d H:i:s');
-        }
-
-        $query = 'SELECT id FROM s_order o
-                LEFT JOIN s_order_details od ON od.orderID = o.id
-                LEFT JOIN s_order_history oh ON oh.orderID = o.id
-                LEFT JOIN s_corepayment_means cp ON cp.id = o.paymentID
-                WHERE cp.name in ('. join(',', $paymentMethods) . ')
-                AND o.status in ('. join(',', $orderStatus) .') 
-                AND oh.change_date >= :changeDate';
-
-        $rows = Shopware()->Db()->fetchAll($query, [':changeDate', $changeDate]);
-        return array_column($rows, 'id');
+        $rows = Shopware()->Db()->fetchAll($query, [
+            ':fullDelivery' =>  $config['RatePayFullDelivery'],
+            ':fullCancellation' => $config['RatePayFullCancellation'],
+            ':fullReturn' => $config['RatePayFullReturn'],
+            ':orderDate' => $orderDate
+        ]);
+        return array_column($rows, 'order_id');
     }
 }
