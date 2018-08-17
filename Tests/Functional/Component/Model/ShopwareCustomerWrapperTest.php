120d7370348ff55643d7d19f69840840d6a02afb
Aaron Whittington <aaron.whittington@ratepay.com>
2018-08-17 13:03:24 +0200

    tests

--- Tests/Functional/Component/Model/ShopwareCustomerWrapperTest.php
+++ Tests/Functional/Component/Model/ShopwareCustomerWrapperTest.php
@@ -8,9 +8,22 @@ use Shopware\Components\Test\Plugin\TestCase;
 class ShopwareCustomerWrapperTest extends TestCase
 {
 
-    public function testGetBilling__fresh()
+    public function testGetBilling()
     {
-        $mock = $this->getCustomerBillingFreshMock();
+        $this->_testGetBilling__fresh();
+        $this->_testGetBillingCountry__fresh();
+
+        if (!class_exists('Shopware\Models\Customer\Billing')) {
+            return;
+        }
+
+        $this->_testGetBilling__rotten();
+        $this->_testGetBillingCountry__rotten();
+    }
+
+    private function _testGetBilling__fresh()
+    {
+        $mock = $this->getCustomerFreshMock();
         $wrapper = new ShopwareCustomerWrapper($mock, Shopware()->Models());
 
         $this->assertEquals(
@@ -26,11 +39,10 @@ class ShopwareCustomerWrapperTest extends TestCase
         );
     }
 
-    public function testGetBillingCountry__fresh()
+    private function _testGetBillingCountry__fresh()
     {
-        $mock = $this->getCustomerBillingFreshMock();
+        $mock = $this->getCustomerFreshMock();
         $wrapper = new ShopwareCustomerWrapper($mock, Shopware()->Models());
-
 
         $country = $wrapper->getBillingCountry();
 
@@ -40,7 +52,107 @@ class ShopwareCustomerWrapperTest extends TestCase
         );
     }
 
-    private function getCustomerBillingFreshMock()
+    private function _testGetBilling__rotten()
+    {
+        $mock = $this->getCustomerRottenMock();
+        $wrapper = new ShopwareCustomerWrapper($mock, Shopware()->Models());
+
+        $this->assertEquals(
+            $wrapper->getBilling('phone'),
+            '1111111111'
+        );
+
+        $billing = $wrapper->getBilling();
+        $this->assertNotNull($billing->getCountryId());
+
+    }
+
+    private function _testGetBillingCountry__rotten()
+    {
+        return;
+        $mock = $this->getCustomerRottenMock();
+        $wrapper = new ShopwareCustomerWrapper($mock, Shopware()->Models());
+
+        $country = $wrapper->getBillingCountry();
+
+        $this->assertEquals(
+            $country->getIso(),
+            'DE'
+        );
+    }
+
+    public function _testGetShipping__fresh()
+    {
+        $mock = $this->getCustomerFreshMock(false);
+        $wrapper = new ShopwareCustomerWrapper($mock, Shopware()->Models());
+
+        $this->assertEquals(
+            $wrapper->getShipping('phone'),
+            '015106483257'
+        );
+
+        $shipping = $wrapper->getShipping();
+
+        $this->assertEquals(
+            $shipping->getCountry()->getIso(),
+            'BC'
+        );
+    }
+
+    public function _testGetShippingCountry__fresh()
+    {
+        $mock = $this->getCustomerFreshMock(false);
+        $wrapper = new ShopwareCustomerWrapper($mock, Shopware()->Models());
+
+        $country = $wrapper->getShippingCountry();
+
+        $this->assertEquals(
+            $country->getIso(),
+            'BC'
+        );
+    }
+
+    public function testGetShipping()
+    {
+        $this->_testGetShipping__fresh();
+        $this->_testGetShippingCountry__fresh();
+
+        if (!class_exists('Shopware\Models\Customer\Billing')) {
+            return;
+        }
+
+        $this->_testGetShipping__rotten();
+        $this->_testGetShippingCountry__rotten();
+    }
+
+    private function _testGetShipping__rotten()
+    {
+        $mock = $this->getCustomerRottenMock(false);
+        $wrapper = new ShopwareCustomerWrapper($mock, Shopware()->Models());
+
+        $this->assertEquals(
+            $wrapper->getShipping('phone'),
+            '1111111111'
+        );
+
+        $shipping = $wrapper->getShipping();
+        $this->assertNotNull($shipping->getCountryId());
+    }
+
+    private function _testGetShippingCountry__rotten()
+    {
+        $mock = $this->getCustomerRottenMock(false);
+        $wrapper = new ShopwareCustomerWrapper($mock, Shopware()->Models());
+
+        $country = $wrapper->getShippingCountry();
+
+        $this->assertEquals(
+            $country->getIso(),
+            'DE'
+        );
+    }
+
+    private function getCustomerFreshMock($billingTest = true)
     {
         $country = new \Shopware\Models\Country\Country();
         $country->setIso('BC');
@@ -51,8 +163,34 @@ class ShopwareCustomerWrapperTest extends TestCase
 
         $stub = $this->createMock('Shopware\Models\Customer\Customer');
 
-        $stub->method('getDefaultBillingAddress')
+        $method = $billingTest ? 'getDefaultBillingAddress' : 'getDefaultShippingAddress';
+
+        $stub->method($method)
              ->willReturn($defaultBillingAddress);
+
+        return $stub;
+    }
+
+    private function getCustomerRottenMock($billingTest = true)
+    {
+        $country = new \Shopware\Models\Country\Country();
+        $country->setIso('BC');
+
+        $defaultBilling = new \Shopware\Models\Customer\Billing();
+        $defaultBilling->setPhone('1111111111');
+
+
+        $country = Shopware()->Models()->getRepository('Shopware\Models\Country\Country')
+            ->findOneBy(['iso' => 'DE']);
+
+        $defaultBilling->setCountryId($country->getId());
+
+        $stub = $this->createMock('Shopware\Models\Customer\Customer');
+
+        $method = $billingTest ? 'getBilling' : 'getShipping';
+
+        $stub->method($method)
+            ->willReturn($defaultBilling);
 
         return $stub;
     }
