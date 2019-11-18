7922ab152022da28ba7f84dab2b8af2a91f19027
Frederik Rommel <frederik.rommel@interlutions.de>
2019-11-18 11:28:31 +0100

    update build file

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir -p build/
-tar -czf build/dist.tar.gz Bootstrapping Component Controller Models Services Views Bootstrap.php plugin.json plugin.png README.md
+tar --exclude-from=.release_exclude  -czf build/dist.tar.gz .
 mkdir -p build/dist/Frontend/RpayRatePay
 tar -xzf build/dist.tar.gz -C build/dist/Frontend/RpayRatePay
 rm -rf build/dist.tar.gz
