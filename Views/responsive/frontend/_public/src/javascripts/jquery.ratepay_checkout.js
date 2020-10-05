744e80793d53d2fe8a8ceeb3cb542b148b407f5d
Frederik Rommel <frederik.rommel@interlutions.de>
2020-10-05 13:10:43 +0200

    RATEPLUG-107: checkout: fix button get not undisabled, after validation

--- Views/responsive/frontend/_public/src/javascripts/jquery.ratepay_checkout.js
+++ Views/responsive/frontend/_public/src/javascripts/jquery.ratepay_checkout.js
@@ -287,6 +287,7 @@
                     if (preLoaderPlugin) {
                         setTimeout(function() {
                             preLoaderPlugin.reset();
+                            $submitButton.prop('disabled', false); // this is just a fix for a shopware bug (has been fixed in a further release)
                         }, 1000);
                     }
                     return false;
