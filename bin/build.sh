1421a4d4de0b0df80ee2bfbaa23a895315b2a547
Frederik Rommel <frederik.rommel@interlutions.de>
2020-10-21 15:59:07 +0200

    update build script

---
+++ bin/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+BASEDIR=$(cd `dirname $0` && pwd)
+PLUGIN_DIR=$(dirname "$BASEDIR")
+PLUGIN_NAME=$(basename "$PLUGIN_DIR")
+BUILD_DIR=$(dirname "PLUGIN_DIR")/build/
+
+mkdir -p build/
+tar -C "$PLUGIN_DIR"/../ --exclude-from="$BASEDIR"/.build_exclude -czf "$BUILD_DIR"/dist.tar.gz "$PLUGIN_NAME"
+rm -rf "$BUILD_DIR"/dist/"$PLUGIN_NAME"
+mkdir -p "$BUILD_DIR"/dist/"$PLUGIN_NAME"
+tar -xzf "$BUILD_DIR"/dist.tar.gz -C "$BUILD_DIR"/dist/
+
+composer remove shopware/shopware --ignore-platform-reqs -o -d "$BUILD_DIR"/dist/"$PLUGIN_NAME"
+
+rm -rf "$BUILD_DIR"/dist.tar.gz
+
+(cd "$BUILD_DIR"/dist && zip -r "$PLUGIN_NAME".zip "$PLUGIN_NAME")
--- build.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p build/
-tar --exclude-from=.release_exclude  -czf build/dist.tar.gz .
-mkdir -p build/dist/RpayRatePay
-tar -xzf build/dist.tar.gz -C build/dist/RpayRatePay
-rm -rf build/dist.tar.gz
-cd build/dist
-zip -r RpayRatePay.zip RpayRatePay
