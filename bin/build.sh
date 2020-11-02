b6b0c16431525e6e8229c87fd2b34c4a4c1d46df
Frederik Rommel <frederik.rommel@interlutions.de>
2020-11-02 09:12:59 +0100

    add composer install to build-script

--- bin/build.sh
+++ bin/build.sh
@@ -5,13 +5,15 @@ PLUGIN_DIR=$(dirname "$BASEDIR")
 PLUGIN_NAME=$(basename "$PLUGIN_DIR")
 BUILD_DIR=$(dirname "PLUGIN_DIR")/build/
 
-mkdir -p build/
+rm -rf "$BUILD_DIR"
+mkdir -p build/dist
 tar -C "$PLUGIN_DIR"/../ --exclude-from="$BASEDIR"/.build_exclude -czf "$BUILD_DIR"/dist.tar.gz "$PLUGIN_NAME"
-rm -rf "$BUILD_DIR"/dist/"$PLUGIN_NAME"
+
 mkdir -p "$BUILD_DIR"/dist/"$PLUGIN_NAME"
 tar -xzf "$BUILD_DIR"/dist.tar.gz -C "$BUILD_DIR"/dist/
 
 composer remove shopware/shopware --ignore-platform-reqs -o -d "$BUILD_DIR"/dist/"$PLUGIN_NAME"
+composer install --no-dev --ignore-platform-reqs -o -d "$BUILD_DIR"/dist/"$PLUGIN_NAME"
 
 rm -rf "$BUILD_DIR"/dist.tar.gz
 
